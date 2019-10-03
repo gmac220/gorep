66b30af7dc32cbb7621eff4a11e9ddc0cdc69086
gmac220 <godfreymacasero@gmail.com>
2019-10-03 14:50:55 -0500

    Before anything goes wrong

--- main.go
+++ main.go
@@ -8,9 +8,15 @@ import (
 )
 
 func main() {
+	var scanner *bufio.Scanner
 	expression, _ := regexp.Compile(os.Args[1])
 
-	scanner := bufio.NewScanner(os.Stdin)
+	if len(os.Args) == 1 {
+		scanner = bufio.NewScanner(os.Stdin)
+	} else {
+		file, _ := os.Open(os.Args[2])
+		scanner = bufio.NewScanner(file)
+	}
 
 	for scanner.Scan() {
 		if expression.MatchString(scanner.Text()) {
